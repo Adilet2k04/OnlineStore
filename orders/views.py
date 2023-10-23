a641a690daeda1f4f782b6b7cd6e1ae5c52beedd
Adilet2k04 <adlet000000@gmail.com>
2023-10-23 15:48:51 +0600

    admin panel upd.

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
-from .models import OrderItem
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 def order_create(request):
@@ -29,3 +30,11 @@ def order_create(request):
     return render(request,
                   'orders/order/create.html',
                   {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order/detail.html',
+                  {'order': order})
